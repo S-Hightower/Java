9beb0ce04fea2ee6008b08a752012dfe8d41c65a
S-Hightower <78988961+S-Hightower@users.noreply.github.com>
2021-11-28 19:07:49 -0700

    Add files via upload

---
+++ Fundamentals/First Java Program/MyInfo.java
@@ -0,0 +1,7 @@
+public class MyInfo {
+	public static void main(String[] args) {
+        System.out.println("My name is Shawna");
+        System.out.println("I am 30 years old");
+        System.out.println("My hometown is Conyers, GA");
+	}
+}
