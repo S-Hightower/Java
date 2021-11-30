259f39beb88fc5fcc6bdf929fc6ff645cde274e5
Shawna Hightower <sdhightower6@gmail.com>
2021-11-29 20:52:13 -0700

    updates

--- Fundamentals/Cafe Business Logic/CafeUtil.java
+++ Fundamentals/Cafe Business Logic/CafeUtil.java
@@ -1,11 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CafeUtil{
 
     public Integer getStreakGoal() {
         int sum = 0;
         for (int i=0; i <= 10; i++){
-            sum += sum + 1;
+            sum += i;
         }
         return sum;
+    }
+
+    public Double getOrderTotal(double[] array) {
+        double total = 0;
+        for (int i = 0; i <= array.length; i++){
+            total += i;
+        }
+        return total;
+    }
+
+    public String displayMenu(String list){
+        String list = loadMenu.get(0);
+
     }
 
 
