7c51f84e8b9c870dbe1a5e086d02942a15c97e2b
S-Hightower <78988961+S-Hightower@users.noreply.github.com>
2021-12-10 16:33:39 -0700

    Add files via upload

---
+++ Spring Boot Projects/book_club/src/main/java/com/shawna/book_club/controllers/HomeController.java
@@ -0,0 +1,70 @@
+package com.shawna.book_club.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.shawna.book_club.models.LoginUser;
+import com.shawna.book_club.models.User;
+import com.shawna.book_club.services.UserService;
+
+@Controller
+public class HomeController {
+	 @Autowired
+	    private UserService userServ;
+	    
+	    @GetMapping("/")
+	    public String index(Model model) {
+	        model.addAttribute("newUser", new User());
+	        model.addAttribute("newLogin", new LoginUser());
+	        return "index.jsp";
+	    }
+	    
+	    @PostMapping("/register")
+	    public String register(@Valid @ModelAttribute("newUser") User newUser, 
+	            BindingResult result, Model model, HttpSession session) {
+	        userServ.register(newUser, result);
+	        if(result.hasErrors()) {
+	            model.addAttribute("newLogin", new LoginUser());
+	            return "index.jsp";
+	        }
+	        session.setAttribute("user_id", newUser.getId());
+	        return "redirect:/home";
+	    }
+	    
+	    @PostMapping("/login")
+	    public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, 
+	            BindingResult result, Model model, HttpSession session) {
+	        User user = userServ.login(newLogin, result);
+	        if(result.hasErrors()) {
+	            model.addAttribute("newUser", new User());
+	            return "index.jsp";
+	        }
+	        session.setAttribute("user_id", user.getId());
+	        session.setAttribute("userName", user.getUserName());
+	        return "redirect:/home";
+	    }
+	    
+	    @GetMapping("/home")
+	    public String homepage(HttpSession session) {
+	    	if(session.getAttribute("user_id")== null) {
+	    		return "redirect:/";
+	    	}else {
+	    	//String userId =(String) session.getAttribute("user_id");
+	    	return "homepage.jsp";
+	    	}
+	    }
+	    
+	    @GetMapping("/logout")
+	    public String logout(HttpSession session) {
+	    	session.invalidate();
+	    	return "redirect:/";
+	    	}
+}
